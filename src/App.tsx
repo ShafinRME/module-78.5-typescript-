bf1d005267e6106e8963016280ae89a2b28fca88
ShafinRME <shafin.rmedu4@gmail.com>
2022-05-31 16:36:54 +0600

    Function parameter types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
+
+let student: string = 'Jolly';
+let age: number = 69;
+let isSmart: boolean = true;
+
+let students: string[] = ['Joe', 'Biden', 'Harris'];
+
+let fees: number[] = [12, 12, 122, 1233, 223, 45];
+
+interface Person {
+  name: string,
+  job?: string,
+  employed: string | boolean,
+  age: number,
+  location?: string | number
+}
+
+const person: Person = {
+  name: 'Shafin',
+  employed: true,
+  job: 'Jobles',
+  age: 85,
+  location: 55
+}
+
+const handleUser = (firstName: string, age: number, address: string): number => {
+  const total: number = 50;
+  console.log(firstName, age, address);
+  return age;
+}
 
 function App() {
   return (
     <div className="App">
-
+      <Counter></Counter>
     </div>
   );
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <h1>This is counter</h1>
+            <h2></h2>
+        </div>
+    );
+};
+
+export default Counter;
